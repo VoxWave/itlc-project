f2c2ef44dca52da78656fcaadec181ce0bd36083
Victor VoxWave Bankowski <victor_bankowski@hotmail.com>
2018-05-14 15:21:58 +0300

    some diary/lecture notes. added tokens to the project. added a exercise.txt file where I will be doing excercises.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
     println!("Hello, world!");
 }
+
+enum Token {
+    Lambda,
+    Dot,
+    Bracket(Direction),
+    Identifier(String),
+}
+
+enum Direction {
+    Right, Left,
+}
+
+struct Lexer {
+
+}
