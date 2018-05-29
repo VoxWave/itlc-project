1c3ecec1cd1fb2699ccb737c26d56a4609135d8c
Your Name <victor_bankowski@hotmail.com>
2018-05-30 01:14:38 +0300

    Parser starting to look like a parser. also a journal entry.
    Expression::Expressions renamed to Expression::Application to express better its purpose in the syntax.
    
    Removed the Sink (and the generics for it) from the parser as the error handling changed to immediately crashing on error. Since the parser only outputs the final expression, differents threads and a pipeline between them would be unnecessary.
    
    Almost all the parts of the parser are done except closing bracket handling (and the handling of when the tokens run out).
    
    Also rambled some stuff about what I've been doing in the journal.

--- src/parser.rs
+++ src/parser.rs
@@ -2,36 +2,30 @@ use common::{Direction, Sink, Source, State};
 use lexer::{LexError, Position, Token, TokenType};
 
 enum Expression {
-    Expressions(Vec<Expression>),
+    Application(Vec<Expression>),
     Lambda(String, Box<Expression>),
-    Identifier(String),
+    Variable(String),
 }
 
 enum Incomplete {
     Expressions(Vec<Expression>),
-    Lambda(String),
+    Lambda(String, Vec<Expression>),
 }
 
-struct Parser<O>
-where
-    O: Sink<Result<Expression, ParserError>>,
+struct Parser
 {
-    expression_sink: O,
     parse_stack: Vec<Incomplete>,
 }
 
-impl<O> Parser<O>
-where
-    O: Sink<Result<Expression, ParserError>>,
+impl Parser
 {
-    fn new(expression_sink: O) -> Parser<O> {
+    fn new() -> Parser {
         Parser {
-            expression_sink,
             parse_stack: vec![Incomplete::Expressions(Vec::new())],
         }
     }
 
-    fn run<I>(&mut self, mut token_source: I)
+    fn run<I>(&mut self, mut token_source: I) -> Expression
     where
         I: Source<Result<Token, LexError>>,
     {
@@ -52,6 +46,11 @@ where
         if error {
             self.print_errors(token_source);
         }
+        self.construct_expression()
+    }
+
+    fn construct_expression(&mut self) -> Expression {
+        Expression::Variable(String::from("placeholder"))
     }
 
     fn print_errors<I>(&mut self, mut token_source: I)
@@ -69,24 +68,63 @@ where
         panic!("Lexing failed. Aborting");
     }
 
-    fn normal(&mut self, t: Token) -> State<Parser<O>, Token> {
+    fn normal(&mut self, t: Token) -> State<Parser, Token> {
         match t.token_type {
-            TokenType::Bracket(Direction::Left) => {}
-            TokenType::Bracket(Direction::Right) => {}
+            TokenType::Bracket(Direction::Left) => {
+                let incomplete = Incomplete::Expressions(Vec::new());
+                self.parse_stack.push(incomplete);
+                State(Self::normal)
+            }
+            TokenType::Bracket(Direction::Right) => {
+                if self.parse_stack.len() > 1 {
+                    let incomplete = self.parse_stack.pop().unwrap();
+                    match incomplete {
+                        Incomplete::Expressions(v) => {
+                            let expression = if v.len() > 1 {
+                                Expression::Application(v)
+                            } else if let Some(e) = v.pop() {
+                                e
+                            } else {
+                                return State(Self::normal);
+                            };
+                            match self.parse_stack.pop().unwrap() {
+                                Incomplete::Expressions(e) => e.push()
+                                Incomplete::Lambda(_, e) => {},
+                            }
+                        },
+                        Incomplete::Lambda(i, v) => {},
+                    }
+                } else {
+                    println!("Unexpected closing bracket at {:?}", t.position);
+                    panic!();
+                }
+            }
             TokenType::Dot => {
                 println!("A dot was found without a lambda at {:?}", t.position);
                 panic!();
             }
-            TokenType::Identifier(s) => {}
+            TokenType::Identifier(s) => {
+                let expression = Expression::Variable(s);
+                match self.parse_stack.pop() {
+                    Some(incomplete) => {
+                        match incomplete {
+                            Incomplete::Expressions(v) => v.push(expression),
+                            Incomplete::Lambda(_, v) => v.push(expression),
+                        }
+                        self.parse_stack.push(incomplete);
+                    },
+                    None => unreachable!(),
+                }
+                State(Self::normal)
+            },
             TokenType::Lambda => State(Self::lambda),
         }
-        State(Self::normal)
     }
 
-    fn lambda(&mut self, t: Token) -> State<Parser<O>, Token> {
+    fn lambda(&mut self, t: Token) -> State<Parser, Token> {
         match t.token_type {
             TokenType::Identifier(s) => {
-                self.parse_stack.push(Incomplete::Lambda(s));
+                self.parse_stack.push(Incomplete::Lambda(s, Vec::new()));
                 State(Self::expect_dot_or_identifier)
             }
             _ => {
@@ -97,11 +135,11 @@ where
         }
     }
 
-    fn expect_dot_or_identifier(&mut self, t: Token) -> State<Parser<O>, Token> {
+    fn expect_dot_or_identifier(&mut self, t: Token) -> State<Parser, Token> {
         match t.token_type {
             TokenType::Dot => State(Self::normal),
             TokenType::Identifier(s) => {
-                self.parse_stack.push(Incomplete::Lambda(s));
+                self.parse_stack.push(Incomplete::Lambda(s, Vec::new()));
                 State(Self::expect_dot_or_identifier)
             }
             _ => {
